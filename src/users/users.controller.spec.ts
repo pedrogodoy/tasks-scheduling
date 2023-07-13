bf26fc67dcef2209d3ebdec0ce1ad422f2e3f280
Pedro <pedrogh.godoy12@gmail.com>
2023-07-13 09:09:11 -0300

    test: users basic tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersModule } from './users.module';
+import * as request from 'supertest';
 import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository, createConnection, getConnection, getRepository } from "typeorm";
@@ -17,9 +18,6 @@ const createUserDto: CreateUserDto = {
 
 describe('UsersController', () => {
   let usersController: UsersController;
-  let usersService: UsersService;
-  let usersRepository: Repository<User>;
-  const testConnectionName = 'testConnection';
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -78,8 +76,6 @@ describe('UsersController', () => {
     }).compile();
 
     
-
-    usersService = app.get<UsersService>(UsersService);
     usersController = app.get<UsersController>(UsersController);
   });
 
@@ -87,39 +83,4 @@ describe('UsersController', () => {
     expect(usersController).toBeDefined();
   });
 
-  // describe('create()', () => {
-  //   it('should create a user', () => {
-  //     usersController.create(createUserDto);
-  //     expect(usersController.create(createUserDto)).resolves.toEqual({
-  //       id: '1',
-  //       ...createUserDto,
-  //     });
-  //     expect(usersService.create).toHaveBeenCalledWith(createUserDto);
-  //   });
-  // });
-
-  // describe('findAll()', () => {
-  //   it('should find all users ', () => {
-  //     usersController.findAll();
-  //     expect(usersService.findAll).toHaveBeenCalled();
-  //   });
-  // });
-
-  // describe('findOne()', () => {
-  //   it('should find a user', () => {
-  //     expect(usersController.findOne('a9862a34-7a1a-44c2-9f8e-e77e88001cf2')).resolves.toEqual({
-  //       firstName: 'firstName #1',
-  //       lastName: 'lastName #1',
-  //       id: 'a9862a34-7a1a-44c2-9f8e-e77e88001cf2',
-  //     });
-  //     expect(usersService.findOne).toHaveBeenCalled();
-  //   });
-  // });
-
-  // describe('remove()', () => {
-  //   it('should remove the user', () => {
-  //     usersController.remove('2');
-  //     expect(usersService.remove).toHaveBeenCalled();
-  //   });
-  // });
 });
